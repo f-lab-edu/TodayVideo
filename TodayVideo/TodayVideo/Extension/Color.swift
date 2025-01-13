b7105cbc9f42eab1e5d6009737e0d2fd6467769e
NORIKIM <jinagold.k@gmail.com>
2025-01-14 08:57:36 +0900

    fix/색상값 사용 오류 수정 및 리펙토링

--- TodayVideo/TodayVideo/Extension/Color.swift
+++ TodayVideo/TodayVideo/Extension/Color.swift
@@ -8,33 +8,24 @@
 import UIKit
 
 extension UIColor {
-    static var background = UIColor(hex: "#070202") ?? .black
+    // SplashView
+    static let background = UIColor(hex: "#070202") ?? .black
+    static let title1 = UIColor(hex: "#4AFFF3") ?? .red
+    static let title2 = UIColor(hex: "#264CA5") ?? .red
+    static let title3 = UIColor(hex: "#000000") ?? .black
 }
 
 extension UIColor {
     public convenience init?(hex: String) {
-        let red, green, blue, alpha: CGFloat
-
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    red = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    green = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    blue = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    alpha = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: red, green: green, blue: blue, alpha: alpha)
-                    return
-                }
-            }
+        let hexFormatted = hex.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "#", with: "")
+        guard hexFormatted.count == 6,
+              let hexNumber = UInt64(hexFormatted, radix: 16) else {
+            return nil
         }
-
-        return nil
+        let red = CGFloat((hexNumber & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hexNumber & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(hexNumber & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
