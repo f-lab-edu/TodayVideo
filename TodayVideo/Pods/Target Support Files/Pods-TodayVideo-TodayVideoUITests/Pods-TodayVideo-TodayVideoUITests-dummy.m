519e4fe57b0d8bc785452489b38ed20b38f1a4de
NORIKIM <jinagold.k@gmail.com>
2025-01-13 10:12:09 +0900

    feat/cocoapos snapkit 추가

---
+++ TodayVideo/Pods/Target Support Files/Pods-TodayVideo-TodayVideoUITests/Pods-TodayVideo-TodayVideoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TodayVideo_TodayVideoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TodayVideo_TodayVideoUITests
+@end
---
+++ TodayVideo/Pods/Target Support Files/Pods-TodayVideoTests/Pods-TodayVideoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TodayVideoTests : NSObject
+@end
+@implementation PodsDummy_Pods_TodayVideoTests
+@end
