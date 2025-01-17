d5d6f81dbc495a069c5d021d079affbe258ff5e5
NORIKIM <jinagold.k@gmail.com>
2025-01-17 09:43:12 +0900

    feat/익스텐션 추가

---
+++ TodayVideo/TodayVideo/Extension/UIImage+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIImage+Extension.swift
+//  TodayVideo
+//
+//  Created by iOS Dev on 1/16/25.
+//
+
+import UIKit
+
+extension UIImage {
+    static let nextButton = UIImage(named: "next")
+}
